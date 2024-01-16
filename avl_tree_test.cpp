105a8b95a85c543cc09125715188607241ec2a51
Yevhenii <res_2005@ukr.net>
2024-01-16 01:46:42 +0100

    INITIAL COMMIT

--- avl_tree_test.cpp
+++ avl_tree_test.cpp
@@ -1,69 +1,60 @@
-#include "avl_tree.h"
-#include <iostream>
 #include <cassert>
-#include <chrono>
-#include "avl_tree_test.h"
+#include <string>
+#include <sstream>
+
+#include "avl_tree.h"
 
 using namespace std;
 
-void test_clear_get_size(){
-    avl_tree<int, string> tree;
 
+void print_separator() {
+    std::cout << "----------------------" << std::endl;
+}
+
+void test_clear_get_size()
+{
+    avl_tree<int, std::string> tree;
     assert(tree.get_size() == 0);
     assert(tree.empty());
-
     tree.insert(10, "A");
     tree.insert(5, "B");
     tree.insert(15, "C");
     tree.insert(2, "D");
-
-    assert(tree.get_size() == 4);
-
+    tree.insert(8, "E");
+    tree.insert(12, "F");
+    tree.insert(18, "G");
+    assert(tree.get_size() == 7);
     tree.clear();
-
     assert(tree.empty());
     assert(tree.get_size() == 0);
-
     cout << "Clear and get size tests passed" << endl;
 }
 
-void test_insert_find(){
-    avl_tree<int, string> tree;
+void test_insert_get_find()
+{
+    avl_tree<int, std::string> tree;
 
     assert(tree.empty());
     assert(tree.get_size() == 0);
-
-    tree.insert(1, "Apple");
-
+    tree.insert(10, "A");
     assert(tree.is_balanced());
     assert(tree.get_size() == 1);
-
-    tree.insert(4, "Google");
-
+    tree.insert(5, "B");
     assert(tree.is_balanced());
     assert(tree.get_size() == 2);
-
-    tree.insert(81, "Microsoft");
-
+    tree.insert(15, "C");
     assert(tree.is_balanced());
     assert(tree.get_size() == 3);
 
-    assert(tree.find(4));
-    assert(tree.find(1));
-    assert(tree.find(81));
+    assert(tree.find(10));
+    assert(tree.find(5));
+    assert(tree.find(15));
 
     assert(tree.find(11) == false);
-
-    cout << "Insert and find tests passed" << endl;
-}
-
-void test_insert_get(){
-    avl_tree<int, string> tree;
 
     tree.insert(10, "A");
     tree.insert(5, "B");
     tree.insert(15, "C");
-
     assert(tree.is_balanced());
     assert(tree.get_size() == 3);
 
@@ -72,23 +63,21 @@ void test_insert_get(){
     assert(tree[15] == "C");
 
     tree.insert(10, "O");
-
     assert(tree.get_size() == 3);
 
     assert(tree[10] == "O");
 
-    tree.insert(10, "G", [](const string &oldInfo, const string &newInfo){ return oldInfo + newInfo; });
-
+    tree.insert(10, "Grapes");
     assert(tree.get_size() == 3);
 
-    assert(tree[10] == "OG");
+    assert(tree[10] == "Grapes");
 
-    cout << "Insert and get tests passed" << endl;
+    cout << "Insert, find and get tests passed" << endl;
 }
 
-void test_remove(){
-    avl_tree<int, string> tree;
-
+void test_remove()
+{
+    avl_tree<int, std::string> tree;
     tree.insert(10, "A");
     tree.insert(5, "B");
     tree.insert(15, "C");
@@ -120,13 +109,13 @@ void test_remove(){
     assert(!tree.find(10));
     assert(tree.is_balanced());
     assert(tree.get_size() == 3);
-
-    cout << "All tests passed!" << endl;
+    cout << "Remove tests passed!" << endl;
 }
 
-void test_assignment_operator(){
-    avl_tree<int, string> tree;
+void test_assignment_operator()
+{
 
+    avl_tree<int, std::string> tree;
     tree.insert(10, "A");
     tree.insert(5, "B");
     tree.insert(15, "C");
@@ -135,8 +124,7 @@ void test_assignment_operator(){
     tree.insert(12, "F");
     tree.insert(18, "G");
 
-    avl_tree<int, string> tree2;
-
+    avl_tree<int, std::string> tree2;
     assert(tree2.get_size() == 0);
     assert(tree2.empty());
 
@@ -151,21 +139,20 @@ void test_assignment_operator(){
     assert(tree[18] == tree2[18]);
 
     tree2[10] = "HelloWord!";
-
     assert(tree2[10] == "HelloWord!");
     assert(tree[10] == "A");
 
     tree2.remove(10);
-
     assert(tree2.find(10) == false);
     assert(tree.find(10) == true);
 
-    cout << "All tests passed!" << endl;
+    cout << "Assignment_operator tests passed!" << endl;
 }
 
-void test_copy_constructor(){
-    avl_tree<int, string> tree;
+void test_copy_constructor()
+{
 
+    avl_tree<int, std::string> tree;
     tree.insert(10, "A");
     tree.insert(5, "B");
     tree.insert(15, "C");
@@ -174,7 +161,7 @@ void test_copy_constructor(){
     tree.insert(12, "F");
     tree.insert(18, "G");
 
-    avl_tree<int, string> tree2 = tree;
+    avl_tree<int, std::string> tree2 = tree;
 
     assert(tree[10] == tree2[10]);
     assert(tree[5] == tree2[5]);
@@ -189,12 +176,12 @@ void test_copy_constructor(){
     assert(tree2.find(10) == false);
     assert(tree.find(10) == true);
 
-    cout << "All tests passed!" << endl;
+    cout << "Copy constructor tests passed!" << endl;
 }
 
-void test_print(){
-    avl_tree<int, string> tree;
-
+void test_print()
+{
+    avl_tree<int, std::string> tree;
     tree.insert(10, "A");
     tree.insert(5, "B");
     tree.insert(15, "C");
@@ -203,10 +190,12 @@ void test_print(){
     tree.insert(12, "F");
     tree.insert(18, "G");
 
-    ostringstream output;
+    std::ostringstream output;
     output << tree;
-    
-    string expectedOutput =
+    cout << output.str();
+
+    // Check the printed output
+    std::string expectedOutput =
             "            18:G\n"
             "      15:C\n"
             "            12:F\n"
@@ -217,85 +206,11 @@ void test_print(){
 
     assert(output.str() == expectedOutput);
 
-    cout << "Print test passed!" << endl;
-}
-
-void test_get_largest(){
-    avl_tree<int, string> tree;
-
-    tree.insert(10, "A");
-    tree.insert(5, "B");
-    tree.insert(15, "C");
-    tree.insert(2, "D");
-    tree.insert(8, "E");
-    tree.insert(12, "F");
-    tree.insert(18, "G");
-
-    int n = 3;
-    vector<pair<int, string>> largestElements = tree.get_largest(n);
-
-    vector<pair<int, string>> expectedElements = {pair<int, string>(18, "G"), pair<int, string>(15, "C"), pair<int, string>(12, "F")};
-
-    assert(largestElements == expectedElements);
-
-    largestElements = tree.get_largest(0);
-    expectedElements = {};
-
-    assert(largestElements == expectedElements);
-
-    largestElements = tree.get_largest(10);
-    expectedElements = {pair<int, string>(18, "G"), pair<int, string>(15, "C"), pair<int, string>(12, "F"),
-                        pair<int, string>(10, "A"), pair<int, string>(8, "E"), pair<int, string>(5, "B"), pair<int, string>(2, "D")};
-
-    assert(largestElements == expectedElements);
-
-    cout << "Largest tests passed!" << endl;
-}
-
-void test_get_smallest(){
-    avl_tree<int, string> tree;
-
-    tree.insert(10, "A");
-    tree.insert(5, "B");
-    tree.insert(15, "C");
-    tree.insert(2, "D");
-    tree.insert(8, "E");
-    tree.insert(12, "F");
-    tree.insert(18, "G");
-
-    int n = 3;
-    vector<pair<int, string>> smallestElements = tree.get_smallest(n);
-
-    vector<pair<int, string>> expectedElements = {
-            make_pair(2, "D"),
-            make_pair(5, "B"),
-            make_pair(8, "E")};
-
-    assert(smallestElements == expectedElements);
-
-    smallestElements = tree.get_smallest(0);
-    expectedElements = {};
-
-    assert(smallestElements == expectedElements);
-
-    smallestElements = tree.get_smallest(10); // more than in the array
-    expectedElements = {
-            make_pair(2, "D"),
-            make_pair(5, "B"),
-            make_pair(8, "E"),
-            make_pair(10, "A"),
-            make_pair(12, "F"),
-            make_pair(15, "C"),
-            make_pair(18, "G")};
-
-    assert(smallestElements == expectedElements);
-
-    cout << "Smallest tests passed!" << endl;
+    std::cout << "Print test passed!" << std::endl;
 }
 
 void test_for_each(){
-    avl_tree<int, string> tree;
-
+    avl_tree<int, std::string> tree;
     tree.insert(10, "A");
     tree.insert(5, "B");
     tree.insert(15, "C");
@@ -304,16 +219,16 @@ void test_for_each(){
     tree.insert(12, "F");
     tree.insert(18, "G");
 
-    vector<pair<int, string>> elements;
+    std::vector<std::pair<int, std::string>> elements;
 
-    auto accumulateFn = [&elements](const int &key, const string &info)
+    auto accumulateFn = [&elements](const int &key, const std::string &info)
     {
         elements.emplace_back(key, info);
     };
 
     tree.for_each(accumulateFn);
 
-    vector<pair<int, string>> expectedElements = {
+    std::vector<std::pair<int, std::string>> expectedElements = {
             {2, "D"},
             {5, "B"},
             {8, "E"},
@@ -324,12 +239,11 @@ void test_for_each(){
 
     assert(elements == expectedElements);
 
-    cout << "All for_each tests passed!" << endl;
+    std::cout << "For_each tests passed!" << std::endl;
 }
 
 void test_maxinfo_selector(){
-    avl_tree<int, string> tree;
-
+    avl_tree<int, std::string> tree;
     tree.insert(10, "A");
     tree.insert(5, "B");
     tree.insert(15, "C");
@@ -339,9 +253,9 @@ void test_maxinfo_selector(){
     tree.insert(18, "G");
 
     int n = 3;
-    vector<pair<int, string>> selectedElements = maxinfo_selector(tree, n);
+    std::vector<std::pair<int, std::string>> selectedElements = maxinfo_selector(tree, n);
 
-    vector<pair<int, string>> expectedElements = {
+    std::vector<std::pair<int, std::string>> expectedElements = {
             {18, "G"},
             {12, "F"},
             {8, "E"}};
@@ -372,51 +286,54 @@ void test_maxinfo_selector(){
             {14, "F"},
             {8, "E"}};
 
-    cout << "All maxinfo_selector tests passed!" << endl;
+    std::cout << "Maxinfo_selector tests passed!" << std::endl;
 }
 
-void test_word_count(){
-    ifstream voyage("beagle_voyage.txt");
-
-    auto wc = count_words(voyage);
-    // cout << wc.get_size() << endl;
-    // auto most_used_words = maxinfo_selector(wc, 20);
-    // for (const auto &pair : most_used_words)
-    // {
-    //     cout << pair.first << ": " << pair.second << endl;
-    // }
-    cout << "Count words tests passed" << endl;
-}
-
-void time_measurement(){
-    for (int rep = 0; rep < 5; rep++){
-        ifstream is("beagle_voyage.txt");
-        auto start_time = chrono::high_resolution_clock::now();
-        auto wc = count_words(is);
-        auto end_time = chrono::high_resolution_clock::now();
+int test_count_words(){
+    for (int rep = 0; rep < 5; ++rep)
+    {
+        std::ifstream is("beagle_voyage.txt");
+        if (!is)
+        {
+            std::cout << "Error opening input file.\n";
+            return 1;
+        }
+        auto start_time = std::chrono::high_resolution_clock::now();
+        std::string word;
+        avl_tree<std::string, int> wc; // counting word occurrences in the stream
+        while (is >> word)
+        {
+            wc[word]++;
+        }
+        auto end_time = std::chrono::high_resolution_clock::now();
         auto time = end_time - start_time;
-        cout << "Ellapsed time: " << time / chrono::milliseconds(1) << "ms" << endl;
+        std::cout << "Ellapsed time: " << time/std::chrono::milliseconds(1)
+                  << " ms.\n";
     }
+    return 0;
 }
 
-int main()
-{
+
+int main(){
+    print_separator();
     test_clear_get_size();
-    test_insert_find();
-    test_insert_get();
+    print_separator();
+    test_insert_get_find();
+    print_separator();
     test_remove();
+    print_separator();
     test_assignment_operator();
+    print_separator();
     test_copy_constructor();
+    print_separator();
     test_print();
-    test_get_largest();
-    test_get_smallest();
+    print_separator();
     test_for_each();
-    cout
-            << "All tests passed!" << endl;
-
-    // External functions tests
+    print_separator();
+    
     test_maxinfo_selector();
-    test_word_count();
-
-    time_measurement();
+    print_separator();
+    test_count_words();
+    
+    return 0;
 }
