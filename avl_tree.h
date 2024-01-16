acaaadc2f9c2c9b6765f1d89bc7854bb80e57951
Yevhenii <res_2005@ukr.net>
2024-01-16 01:46:20 +0100

    fix: fixed bugs feat: speeded up the word counting

--- avl_tree.h
+++ avl_tree.h
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <chrono>
 #include <vector>
+#include <queue>
+#include <map>
 
 #pragma once
 
@@ -77,31 +79,32 @@ private:
         if (src == nullptr) return nullptr;
 
         Node *new_node = new Node(src->key, src->info);
-        new_node->left = copyHelper(src->left);
-        new_node->right = copyHelper(src->right);
+        new_node->left = copy_helper(src->left);
+        new_node->right = copy_helper(src->right);
         new_node->height = src->height;
 
         return new_node;
     }
 
-    Node *insert_helper(Node*& node, const Key& key, const Info& info)
+    void insert_helper(Node*& node, const Key& key, const Info& info, Node*& found_node)
     {
         if(node == nullptr){
             size++;
-            return new Node(key, info);
+            node = new Node(key, info);
+            found_node = node;
         }
 
-        if(key < node->key){
-            node->left = insert_helper(node->left, key, info);
+        if(key < node->key) {
+            insert_helper(node->left, key, info, found_node);
+        } else if(key > node->key){
+            insert_helper(node->right, key, info, found_node);
         } else{
-            throw std::invalid_argument("Duplicate key found");
             // if duplicate is found, update the info
             node->info = info;
-            return node;
         }
 
-        update_height();
-        return balance(node);
+        update_height(node);
+        node = balance(node);
     }
 
     Node* balance(Node* node){
@@ -153,72 +156,53 @@ private:
         return new_root;
     }
 
-    Node *findMin(Node *node) const
-    {
-        if (!node->left)
-        {
-            return node;
-        }
-        return findMin(node->left);
+    Node* find_min(Node* node) const{
+        if (!node->left) return node;
+
+        return find_min(node->left);
     }
 
-    Node *findNode(Node *node, const Key &key) const
-    {
+    Node* find_max(Node* node) const{
+        if(!node->left) return node;
 
-        if (node == nullptr || key == node->key)
-        {
-            return node;
-        }
-
-        if (key < node->key)
-        {
-            return findNode(node->left, key);
-        }
-        else
-        {
-            return findNode(node->right, key);
-        }
+        return find_max(node->right);
     }
 
-    bool removeHelper(Node *&node, const Key &key)
+    Node* find_node(Node* node, const Key& key) const{
+        if (node == nullptr || key == node->key) return node;
+
+        if (key < node->key) return find_node(node->left, key);
+
+        else return find_node(node->right, key);
+    }
+
+    bool remove_helper(Node *&node, const Key &key)
     {
-        if (!node)
-        {
-            return false; // node not found
-        }
+        if (!node) return false;
+        
         bool deleted = false;
-        if (node->key > key)
-        {
-            deleted = removeHelper(node->left, key);
-        }
-        else if (node->key < key)
-        {
-            deleted = removeHelper(node->right, key);
-        }
-        else
-        {
-            if (!node->left || !node->right)
-            {
+        
+        if (node->key > key) { deleted = remove_helper(node->left, key); }
+
+        else if (node->key < key) { deleted = remove_helper(node->right, key); }
+
+        else{
+            if (!node->left || !node->right){
                 Node *temp = node->left ? node->left : node->right;
 
-                if (!temp) // no child
-                {
+                if (!temp){
                     temp = node;
                     node = nullptr;
                 }
-                else // one child
-                {
-                    *node = *temp; // Copy the content of the non-empty child
-                }
+                else { *node = *temp; }
 
                 delete temp;
             }
-            else
-            {
-                Node *successor = findMin(node->right);
+            else{
+                Node *successor = find_min(node->right);
                 node->key = successor->key;
                 node->info = successor->info;
-                removeHelper(node->right, successor->key);
+                remove_helper(node->right, successor->key);
             }
 
             deleted = true;
@@ -244,58 +228,38 @@ private:
         }
     }
 
+    template <typename Fn> void traverse(const Node* node, Fn fn) const {
+        if (node) {
+            traverse(node->left, fn);
+            fn(node->key, node->info);
+            traverse(node->right, fn);
+        }
+    }
+
 public:
-    // Constructor
-    avl_tree(){};
+    avl_tree() {}
 
-    // Copy constructor
-    avl_tree(const avl_tree &src)
-    {
-        *this = src;
-    }
+    avl_tree(const avl_tree& src) { *this = src; }
 
-    // Destructor
-    ~avl_tree()
-    {
-        clear();
-    }
+    ~avl_tree() { clear(); }
 
-    // Overloaded assignment operator
-    avl_tree &operator=(const avl_tree &src)
-    {
-        if (this != &src)
-        {
+    avl_tree& operator=(const avl_tree& src){
+        if (this != &src){
             clear();
-            root = copyHelper(src.root);
+            root = copy_helper(src.root);
             this->size = src.size;
         }
 
         return *this;
     }
 
-    template <typename Fn>
-    void for_each(Fn fn) { for_each(root, fn); }
-    vector<pair<Key, Info>> getLargest(int n)
-    {
-        std::vector<pair<Key, Info>> result;
-        getLargestHelper(root, n, result);
-        return result;
-    }
+    template <typename Fn>void for_each(Fn fn) { for_each(root, fn); }
 
-    vector<pair<Key, Info>> getSmallest(int n)
-    {
-        std::vector<pair<Key, Info>> result;
-        getSmallestHelper(root, n, result);
-        return result;
-    }
-
-    bool empty() const
-    {
+    bool empty() const{
         return size == 0;
     }
 
-    int get_size() const
-    {
+    int get_size() const{
         return size;
     }
 
@@ -303,8 +267,7 @@ public:
      * @brief removes all elements from avl tree
      *
      */
-    void clear()
-    {
+    void clear(){
         clear_helper(root);
         root = nullptr;
     }
@@ -316,13 +279,9 @@ public:
      * @param info is info that will be inserted
      * @param onKeyExists is function that will be called if key already exists, by default function returns new info
      */
-    void insert(
-            const Key &key, const Info &info,
-            function<Info(const Info &oldInfo, const Info &newInfo)> onKeyExists =
-            [](const Info &oldInfo, const Info &newInfo)
-            { return newInfo; })
-    {
-        root = insertHelper(root, key, info, onKeyExists);
+    void insert(const Key& key, const Info& info) {
+        Node* found_node;
+        insert_helper(root, key, info, found_node);
     }
 
     /**
@@ -332,10 +291,8 @@ public:
      * @return true if element was removed
      * @return false if element not exists
      */
-    bool remove(const Key &key)
-    {
-        if (removeHelper(root, key))
-        {
+    bool remove(const Key& key){
+        if (remove_helper(root, key)){
             size--;
             return true;
         }
@@ -349,9 +306,8 @@ public:
      * @return true if element found
      * @return false if element not found
      */
-    bool find(const Key &key) const
-    {
-        return findNode(root, key) != nullptr;
+    bool find(const Key& key) const{
+        return find_node(root, key) != nullptr;
     }
 
     /**
@@ -360,12 +316,10 @@ public:
      * @param key is the key that will be searched
      * @return Info& info associated with the key
      */
-    Info &operator[](const Key &key)
-    {
-        Node *node = findNode(root, key);
-        if (node == nullptr)
-        {
-            throw std::runtime_error("Key not found");
+    Info& operator[](const Key& key){
+        Node *node = find_node(root, key);
+        if (node == nullptr){
+            insert_helper(root, key, Info(), node);
         }
         return node->info;
     }
@@ -388,58 +342,50 @@ public:
 
     friend std::ostream &operator<<(std::ostream &os, const avl_tree &tree)
     {
-        if (tree.get_size() > 40)
-        {
-            os << "Tree is too big to print";
-            return os;
-        }
         tree.printTree(os, tree.root, 0);
         return os;
     }
 
     // Function designed just for testing
-    bool isBalanced()
-    {
-        return isBalancedHelper(root);
+    bool is_balanced() { return is_balanced_helper(root); }
+
+    template<typename Fn> void traverse(Fn fn) const{
+        traverse(root, fn);
     }
-    //
 };
 
 // External methods
 
 template <typename Key, typename Info>
-std::vector<std::pair<Key, Info>> maxinfo_selector(const avl_tree<Key, Info> &tree, unsigned cnt)
-{
-    avl_tree<pair<Info, Key>, int> inverted;
+std::vector<std::pair<Key, Info>> maxinfo_selector(const avl_tree<Key, Info>& tree, unsigned cnt) {
+    std::vector<std::pair<Key, Info>> result;
 
-    auto copy = tree;
+    // Initialize priority queue to store elements based on info values
+    std::priority_queue<std::pair<Info, Key>> pq;
 
-    auto accumulateFn = [&inverted](const Key &key, const Info &info)
-    {
-        inverted.insert(make_pair(info, key), 1);
-    };
+    // Perform in-order traversal and populate the priority queue
+    tree.traverse([&](const Key& key, const Info& info) {
+        pq.push({info, key});
+    });
 
-    copy.for_each(accumulateFn);
-
-    vector<pair<pair<Info, Key>, int>> largest = inverted.getLargest(cnt);
-
-    vector<pair<Key, Info>> aboba;
-    for (auto &pair : largest)
-    {
-        aboba.push_back(make_pair(pair.first.second, pair.first.first));
+    // Pop cnt elements with the highest info values from the priority queue
+    while (!pq.empty() && cnt > 0) {
+        auto info_key = pq.top();
+        result.push_back({info_key.second, info_key.first});
+        pq.pop();
+        cnt--;
     }
 
-    return aboba;
+    return result;
 }
 
-avl_tree<string, int> count_words(istream &is)
-{
+avl_tree<std::string ,int> count_words(std::istream& is){
+    avl_tree<std::string,int> treeRes;
+
     std::string word;
-    avl_tree<string, int> wc;
-    while (is >> word)
-    {
-        wc.insert(word, 1, [](const int &oldValue, const int &newValue)
-        { return oldValue + newValue; });
+    while(is >> word){
+        treeRes[word]++;
     }
-    return wc;
+
+    return treeRes;
 }
